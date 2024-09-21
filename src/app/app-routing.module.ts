e509025bffa45df9ce3d7aeb2408ffa1bbc7081a
leandroVR1 <yeifry.121@gmail.com>
2024-09-20 22:05:32 -0500

    fronted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full' },
+  {path: 'login', component:LoginComponent},
+  {path: 'signIn', component: SignInComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '**',  redirectTo: 'login', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
